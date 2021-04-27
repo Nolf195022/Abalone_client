79666520034ca726bd77ad2dfbd7cd10b978fde7
Tanguy Nolf <195022@ecam.be>
2021-04-27 12:56:08 +0200

    Multiple ports addition

--- abalon-client.py
+++ abalon-client.py
@@ -3,17 +3,6 @@ import json
 import sys
 from threading import Thread
 
-#Donner l'option d'entrée un nom de port lors de l'éxecution du script
-try:
-    port = int(sys.argv [1])
-except:
-    port = 4000
-try:
-    pseudo = sys.argv [2]
-except:
-        pseudo = 'PLayer 1'
-
-
 def receiveJSON(socket):
     fullreceive = False
     message = ''
@@ -33,32 +22,41 @@ def sendJSON(socket, data):
         sent = socket.send(data[totalsend:])
         totalsend += sent
 
-message = {"request": "subscribe","port": port,"name": pseudo,"matricules": ["195022", "02305"]}
 
-s = socket.socket()
-s.connect(('localhost',3000))
-sendJSON(s, message)
-s.close()
-#boucle de réception de requêtes
-while True: 
-    s = socket.socket()
-    s.bind(('0.0.0.0', port))
-    s.listen()
-    client, address = s.accept()
-    serverrequest = receiveJSON(client)
-    print(serverrequest)
-    if serverrequest['request'] == 'ping':
-        sendJSON(client, {'response': 'pong'})
-    if serverrequest['request'] == 'play':
-        # 'current': 0 -> black, 'current': 1 -> white
-        if serverrequest['state']['current'] == 0:
-            move = {"marbles": [[6, 4], [6, 5]],"direction": "W"}
-            moverequest = {"response": "move","move": move,"message": "Fun message black"}
-        if serverrequest['state']['current'] == 1:
-            move = {"marbles": [[2, 2], [2, 3], [2, 4]],"direction": "W"}
-            moverequest = {"response": "move","move": move,"message": "Fun message white"}
+playernames = ['Albert','Patrick']
+playernumber = 1
+if len(sys.argv) > 1:
+    for port in sys.argv[1:]:
+        port = int(port)
+        message = {"request": "subscribe","port": port,"name": playernames[playernumber-1],"matricules": ["19022", "18164"]}
+        playernumber += 1
+        s = socket.socket()
+        s.connect(('localhost',3000))
+        sendJSON(s, message)
+        s.close()
+        #boucle de réception de requêtes
+        listening = True
+        while listening == True:
+            s = socket.socket()
+            s.bind(('0.0.0.0', port))
+            s.listen()
+            client, address = s.accept()
+            serverrequest = receiveJSON(client)
+            print(serverrequest)
+            if serverrequest['request'] == 'ping':
+                sendJSON(client, {'response': 'pong'})
+                listening = False
+
+#     if serverrequest['request'] == 'play':
+#         # 'current': 0 -> black, 'current': 1 -> white
+#         if serverrequest['state']['current'] == 0:
+#             move = {"marbles": [[6, 4], [6, 5]],"direction": "W"}
+#             moverequest = {"response": "move","move": move,"message": "Fun message black"}
+#         if serverrequest['state']['current'] == 1:
+#             move = {"marbles": [[2, 2], [2, 3], [2, 4]],"direction": "W"}
+#             moverequest = {"response": "move","move": move,"message": "Fun message white"}
         
-        sendJSON(client, moverequest)
+#         sendJSON(client, moverequest)
 
 # 'board': [
 # 			['W', 'W', 'W', 'W', 'W', 'X', 'X', 'X', 'X'],
